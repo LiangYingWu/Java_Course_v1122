ce522bee7390f05b5b802b378b461f8ad21a7cbc
LiangYingWu <jackdaw5487@gmail.com>
2025-03-01 16:27:37 +0800

    Lab19

--- FCPS_eUnits/Unit1/src/Dorothy.java
+++ FCPS_eUnits/Unit1/src/Dorothy.java
@@ -1,3 +1,41 @@
+import edu.fcps.karel2.Display;
+
 public class Dorothy extends Athlete{
-    
+    public Dorothy(){
+        super(2, 2, Display.EAST, 0);
+    }
+    public boolean findPath(){
+        move();
+        boolean next_to_beeper = nextToABeeper();
+        if(!nextToABeeper()){
+            turnAround();
+            move();
+        }
+        return next_to_beeper;
+    }
+    public void followPath(){
+        while(true){
+            if(frontIsClear()){
+                if(!findPath()){
+                    turnLeft();
+                    if(frontIsClear()){
+                        if(!findPath()){
+                            if(frontIsClear()){
+                                if(!findPath()){
+                                    turnLeft();
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    else{
+                        turnAround();
+                    }
+                }
+            }
+            else{
+                turnLeft();
+            }
+        }
+    }
 }
