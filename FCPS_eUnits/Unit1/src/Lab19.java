57ee9f4468e35b7e30b6bede6bf6380194bcafed
LiangYingWu <jackdaw5487@gmail.com>
2025-03-01 16:28:22 +0800

    Lab19

--- FCPS_eUnits/Unit1/src/Lab19.java
+++ FCPS_eUnits/Unit1/src/Lab19.java
@@ -4,8 +4,8 @@ import javax.swing.JOptionPane;
 public class Lab19 {
     public static void main(String[] args){
         String filename = JOptionPane.showInputDialog("What robot world?");
-        // Display.openWorld("maps/"+filename+".map");
-        Display.openWorld("FCPS_eUnits/Unit1/maps/"+filename+".map");
+        Display.openWorld("maps/"+filename+".map");
+        // Display.openWorld("FCPS_eUnits/Unit1/maps/"+filename+".map");
         Display.setSpeed(10);
 
         Dorothy apple = new Dorothy();
